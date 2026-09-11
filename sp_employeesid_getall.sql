a183cedf0a1ffcf63920ab627c490a547b31e9ca
Mohd Gulfam <gulfam.bottomhalf@gmail.com>
2026-09-11 20:56:22 +0530

    Update sp_employeesid_getall.sql

--- sp_employeesid_getall.sql
+++ sp_employeesid_getall.sql
@@ -1,23 +1,51 @@
--- DROP FUNCTION public.sp_employeesid_getall();
+DROP PROCEDURE IF EXISTS public.sp_employeesid_getall(
+    refcursor
+);
 
-CREATE OR REPLACE FUNCTION public.sp_employeesid_getall()
- RETURNS SETOF employees
- LANGUAGE plpgsql
-AS $function$
+CREATE OR REPLACE PROCEDURE public.sp_employeesid_getall(
+    INOUT _result refcursor
+)
+LANGUAGE plpgsql
+AS $procedure$
 DECLARE
     _sqlstate TEXT;
     _errorno TEXT;
     _errortext TEXT;
     _message TEXT;
-    _result TEXT;
+    _logresult TEXT;
 BEGIN
-    RETURN QUERY select * from employees;
+
+    OPEN _result FOR
+        SELECT *
+        FROM employees;
+
 EXCEPTION WHEN OTHERS THEN
+
     _sqlstate := SQLSTATE;
     _errortext := SQLERRM;
     _errorno := SQLSTATE;
-    _message := concat('ERROR ', _errorno, ' (', _sqlstate, '): ', _errortext);
-    CALL sp_logexception(_message, '', 'sp_employeesid_getall', 1, 0, _result);
+
+    _message := concat(
+        'ERROR ',
+        _errorno,
+        ' (',
+        _sqlstate,
+        '): ',
+        _errortext
+    );
+
+    CALL sp_logexception(
+        _message,
+        '',
+        'sp_employeesid_getall',
+        1,
+        0,
+        _logresult
+    );
+
+    RAISE EXCEPTION
+        'Error in sp_employeesid_getall: %',
+        _message;
+
 END;
-$function$
-;
+$procedure$;
