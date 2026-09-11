7689b6f3fb9b842e7a945f5e9dafb0bcca15792d
Mohd Gulfam <gulfam.bottomhalf@gmail.com>
2026-09-11 20:44:56 +0530

    Update sp_email_templates_insupd.sql

--- sp_email_templates_insupd.sql
+++ sp_email_templates_insupd.sql
@@ -1,23 +1,36 @@
--- DROP FUNCTION public.sp_email_templates_insupd(int4, varchar, varchar, int8);
+DROP PROCEDURE IF EXISTS public.sp_email_templates_insupd(
+    integer,
+    varchar,
+    varchar,
+    bigint,
+    INOUT varchar
+);
 
-CREATE OR REPLACE FUNCTION public.sp_email_templates_insupd(_emailtemplateid integer, _templatename character varying, _sampletemplateurl character varying, _createdby bigint)
- RETURNS character varying
- LANGUAGE plpgsql
-AS $function$
+CREATE OR REPLACE PROCEDURE public.sp_email_templates_insupd(
+    IN _emailtemplateid integer,
+    IN _templatename varchar,
+    IN _sampletemplateurl varchar,
+    IN _createdby bigint,
+    INOUT _processingresult varchar
+)
+LANGUAGE plpgsql
+AS $procedure$
 DECLARE
     _sqlstate TEXT;
     _errorno TEXT;
     _errortext TEXT;
     _message TEXT;
     _result TEXT;
-    _processingresult character varying;
 BEGIN
+
     _processingresult := '';
 
     IF NOT EXISTS (
-        SELECT 1 FROM email_templates 
+        SELECT 1
+        FROM email_templates
         WHERE emailtemplateid = _emailtemplateid
     ) THEN
+
         INSERT INTO email_templates (
             templatename,
             sampletemplateurl,
@@ -32,26 +45,44 @@ BEGIN
         );
 
         _processingresult := 'inserted';
+
     ELSE
-        UPDATE email_templates SET
+
+        UPDATE email_templates
+        SET
             templatename = _templatename,
             sampletemplateurl = _sampletemplateurl
         WHERE emailtemplateid = _emailtemplateid;
 
         _processingresult := 'updated';
+
     END IF;
-    
-    -- Return the result directly
-    RETURN _processingresult;
 
 EXCEPTION WHEN OTHERS THEN
+
     _sqlstate := SQLSTATE;
     _errortext := SQLERRM;
     _errorno := SQLSTATE;
-    _message := concat('ERROR ', _errorno, ' (', _sqlstate, '): ', _errortext);
-    -- It is perfectly fine to CALL your logging procedure from inside a function
-    CALL sp_logexception(_message, '', 'sp_email_templates_insupd', 1, 0, _result);
-    RETURN 'error';
+
+    _message := concat(
+        'ERROR ',
+        _errorno,
+        ' (',
+        _sqlstate,
+        '): ',
+        _errortext
+    );
+
+    CALL sp_logexception(
+        _message,
+        '',
+        'sp_email_templates_insupd',
+        1,
+        0,
+        _result
+    );
+
+    _processingresult := 'error';
+
 END;
-$function$
-;
+$procedure$;
