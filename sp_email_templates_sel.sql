ab3e61c24e9cfa3dc10847e98af8acea85f297e5
Mohd Gulfam <gulfam.bottomhalf@gmail.com>
2026-09-11 20:52:56 +0530

    Update sp_email_templates_sel.sql

--- sp_email_templates_sel.sql
+++ sp_email_templates_sel.sql
@@ -1,27 +1,40 @@
--- DROP FUNCTION public.sp_email_templates_sel(varchar, int4, int4);
+DROP PROCEDURE IF EXISTS public.sp_email_templates_sel(
+    varchar,
+    integer,
+    integer,
+    refcursor
+);
 
-CREATE OR REPLACE FUNCTION public.sp_email_templates_sel(_templatename character varying, _pagenumber integer, _pagesize integer)
- RETURNS TABLE(emailtemplateid integer, templatename character varying, sampletemplateurl character varying, createdon timestamp with time zone, createdby bigint)
- LANGUAGE plpgsql
-AS $function$
+CREATE OR REPLACE PROCEDURE public.sp_email_templates_sel(
+    IN _templatename varchar,
+    IN _pagenumber integer,
+    IN _pagesize integer,
+    INOUT _result refcursor
+)
+LANGUAGE plpgsql
+AS $procedure$
 DECLARE
     _offset integer;
 BEGIN
+
     -- Calculate the offset for pagination
     _offset := (_pagenumber - 1) * _pagesize;
 
-    RETURN QUERY 
-    SELECT
-        e.emailtemplateid,
-        e.templatename,
-        e.sampletemplateurl,
-        e.createdon,
-        e.createdby
-    FROM email_templates e
-    WHERE (_templatename IS NULL OR e.templatename ILIKE concat('%', _templatename, '%'))
-    ORDER BY e.emailtemplateid DESC
-    LIMIT _pagesize 
-    OFFSET _offset;
+    OPEN _result FOR
+        SELECT
+            e.emailtemplateid,
+            e.templatename,
+            e.sampletemplateurl,
+            e.createdon,
+            e.createdby
+        FROM email_templates e
+        WHERE (
+            _templatename IS NULL
+            OR e.templatename ILIKE CONCAT('%', _templatename, '%')
+        )
+        ORDER BY e.emailtemplateid DESC
+        LIMIT _pagesize
+        OFFSET _offset;
+
 END;
-$function$
-;
+$procedure$;
