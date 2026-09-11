b326ef213d4a9c77104082795a95be9a237a7d84
Mohd Gulfam <gulfam.bottomhalf@gmail.com>
2026-09-11 21:00:18 +0530

    Update sp_employees_by_offset.sql

--- sp_employees_by_offset.sql
+++ sp_employees_by_offset.sql
@@ -1,14 +1,29 @@
-CREATE OR REPLACE FUNCTION public.sp_employees_by_offset(_pagesize integer, _offsetsize integer)
- RETURNS SETOF employees
- LANGUAGE plpgsql
-AS $function$
+CREATE OR REPLACE PROCEDURE public.sp_employees_by_offset(
+    IN _pagesize integer,
+    IN _offsetsize integer,
+    INOUT _result_cursor refcursor
+)
+LANGUAGE plpgsql
+AS $procedure$
 BEGIN
+    -- Convert page/offset value
     _offsetsize := _offsetsize - 1;
-    
-    RETURN QUERY 
-    SELECT * FROM employees e    -- FIXED: Changed to SELECT * to match 'SETOF employees'
-    WHERE isactive = true        -- FIXED: Changed 1 to true for the boolean column
-    ORDER BY employeeuid
-    LIMIT _pagesize OFFSET _offsetsize;
+
+    OPEN _result_cursor FOR
+    SELECT *
+    FROM employees e
+    WHERE e.isactive = true
+    ORDER BY e.employeeuid
+    LIMIT _pagesize
+    OFFSET _offsetsize;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        CALL sp_logexception(
+            SQLERRM,
+            'sp_employees_by_offset'
+        );
+
+        RAISE EXCEPTION '%', SQLERRM;
 END;
-$function$;
+$procedure$;
